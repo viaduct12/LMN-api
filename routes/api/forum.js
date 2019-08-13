b581326fed8d64c7f76e21d2e25c2a5dd9d01807
Meghan Mullally <ms.mullally27@gmail.com>
2019-08-13 15:02:54 -0700

    created forum, post, index, code to create routing

---
+++ routes/api/forum.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const db = require('../../models');
+
+// api forum using all the categories 
+router.get("/:category", (req, res) => {
+  const forumCategory = req.params.category;
+  db.Forum.findAll({where: {forumCategory: forumCategory}}).then(result => {
+    res.json(result);
+  })
+});
+
+
+
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,14 @@
 const router = require("express").Router();
 const scrapeRoutes = require('./scrape');
 const userRoutes = require("./userAuth");
+const forumRoutes = require("./forum");
 
 // Register public auth api routes
 router.use("/auth", userRoutes);
 
 // Register book routes
 router.use("/article", scrapeRoutes);
+
+router.use("/forum", forumRoutes);
 
 module.exports = router;
